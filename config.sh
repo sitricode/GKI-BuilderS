a229e1db4b634e5579b46d7a97c4b61bd9dbeb6d
sitricode <sitricode@outlook.co.id>
2026-06-28 14:45:10 +0700

    Update Clang URL and branch in config.sh

--- config.sh
+++ config.sh
@@ -16,5 +16,5 @@ KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
 GKI_RELEASES_REPO=""
 # Clang
-CLANG_URL="https://pub-518e5c4602fc4016aa4a80856eaa0ad4.r2.dev/clang-r536225.tar.gz"
-CLANG_BRANCH=""
+CLANG_URL="https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
+CLANG_BRANCH="lineage-20.0"
