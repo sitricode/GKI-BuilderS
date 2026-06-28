42d9af73559d5a7cc7c3522bcb85060ac691f10a
sitricode <sitricode@outlook.co.id>
2026-06-28 07:08:46 +0700

    Update kernel configuration variables and URLs

--- config.sh
+++ config.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Kernel name
-KERNEL_NAME="SuiKernel"
+KERNEL_NAME="Miharu"
 # Kernel Build variables
-USER="KanagawaYamada"
-HOST="HoshimachiSuisei"
+USER="nigga"
+HOST="build-host"
 TIMEZONE="Asia/Jakarta"
 # AnyKernel
 ANYKERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-anykernel"
 ANYKERNEL_BRANCH="gki"
 # Kernel Source
-KERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-android12-5.10"
-KERNEL_BRANCH="${KERNEL_BRANCH_ENV:-suikernel-stable}"
+KERNEL_REPO="https://github.com/sitricode/gki_5.10"
+KERNEL_BRANCH="${KERNEL_BRANCH_ENV:-master}"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
-GKI_RELEASES_REPO="https://github.com/LoggingNewMemory/SuiKernel-Release"
+GKI_RELEASES_REPO=""
 # Clang
-CLANG_URL="https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
-CLANG_BRANCH="lineage-20.0"
+CLANG_URL="https://pub-518e5c4602fc4016aa4a80856eaa0ad4.r2.dev/clang-r536225.tar.gz"
+CLANG_BRANCH=""
