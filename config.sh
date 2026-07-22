232a22ad5c49bc26aca3efe6938257db6d9139eb
sitricode <sitricode@outlook.co.id>
2026-07-22 16:29:56 +0700

    Modify Clang configuration in config.sh
    Updated Clang URL and commented out old definitions.

--- config.sh
+++ config.sh
@@ -15,6 +15,9 @@ KERNEL_BRANCH="${KERNEL_BRANCH_ENV:-yuuka-main}"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
 GKI_RELEASES_REPO=""
-# Clang
-CLANG_URL="https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
-CLANG_BRANCH="lineage-20.0"
+# Clang https://pub-518e5c4602fc4016aa4a80856eaa0ad4.r2.dev/clang-r536225.tar.gz
+#CLANG_URL="https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
+#CLANG_BRANCH="lineage-20.0"
+
+CLANG_URL="https://pub-518e5c4602fc4016aa4a80856eaa0ad4.r2.dev/clang-r536225.tar.gz"
+CLANG_BRANCH=""
